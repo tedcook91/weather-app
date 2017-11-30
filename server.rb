f033f70a5023b210dc82eb1ef00fe7731b1129ec
captainangus <edward.w.cook@gmail.com>
2017-11-30 15:23:16 -0500

    api key

--- server.rb
+++ server.rb
@@ -1,2 +1,12 @@
 require "sinatra"
 require "net/http"
+
+get '/' do
+  "hello world"
+end
+
+
+get "/api/v1/forecast/:latitude,:longitude" do |latitude, longitude|
+  uri = URI("https://api.darksky.net/forecast/#{DARK_SKY_API_KEY}/#{latitude},#{longitude}")
+  return Net::HTTP.get(uri)
+end
